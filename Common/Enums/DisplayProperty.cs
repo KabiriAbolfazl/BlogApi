b64cb52b1dac4c3a88e4890894e62ff175a4758a
Abolfazl Kabiri <Abolfazl_Kabiri@outlook.com>
2022-01-25 23:22:35 +0330

    add some common utilites

---
+++ Common/Enums/DisplayProperty.cs
@@ -0,0 +1,12 @@
+﻿namespace Common.Enums
+{
+    public enum DisplayProperty
+    {
+        Description,
+        GroupName,
+        Name,
+        Prompt,
+        ShortName,
+        Order
+    }
+}
